9cf06ffc4cb8d8320f7bd1ffcb4137c8f13123a1
aleja <aleja@DESKTOP-HO58E9V.localdomain>
2024-11-11 09:49:57 -0600

    prueba

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b5f3ad86-e285-407e-a13a-62ba31be2d5d</testSuiteGuid>
+</TestSuiteEntity>
